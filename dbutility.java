4c4c1748ea793b272a28c7af131c5fc911334018
hari6214 <hariprasadreddy12@gmail.com>
2022-09-19 16:33:53 +0530

    lastup

--- dbutility.java
+++ dbutility.java
@@ -1,13 +1,3 @@
 hi
 hello
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-my name is hari
-=======
-how r u
->>>>>>> 579e077050d005e013fb1ff46ce17f4dd3ab86a6
-=======
->>>>>>> parent of 579e077 (Update dbutility.java)
-=======
->>>>>>> parent of 579e077 (Update dbutility.java)
+this is updated in the specific file
